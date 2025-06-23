4c2521dd0ef2bba7c1eaae37ca56e280b4473390
aliafzalh <aliafzalh@gmail.com>
2025-06-22 22:07:31 -0400

    new file added

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv');
+const connectDB = require('./db');
 
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+//Connect to mongoDB
+connectDB();
+app.use(express.json());
 
 app.get(`/`, (req, res) => {
     res.send(`Hello project api`)
